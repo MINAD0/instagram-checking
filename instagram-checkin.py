0c6a6175840c4dfa820e5b8e89a83f711e16032c
MINADO <al0mahdi.ait1ounzar@gmail.com>
2023-07-17 23:19:11 +0100

    adding something new like unfollow users

--- instagram-checkin.py
+++ instagram-checkin.py
@@ -1,5 +1,6 @@
 from instaloader import Instaloader, Profile
 from colorama import init, Fore
+from instagram_private_api import Client, ClientCompatPatch
 from tqdm import tqdm
 import time
 
@@ -15,7 +16,8 @@ menu_design = f"""
 {Fore.GREEN}║     {Fore.CYAN}1. Check Followers               {Fore.GREEN}║
 {Fore.GREEN}║     {Fore.CYAN}2. Check Following               {Fore.GREEN}║
 {Fore.GREEN}║     {Fore.CYAN}3. Find Unfollowers              {Fore.GREEN}║
-{Fore.GREEN}║     {Fore.CYAN}4. Exit                          {Fore.GREEN}║
+{Fore.GREEN}║     {Fore.CYAN}4. Unfollow All Users            {Fore.GREEN}║
+{Fore.GREEN}║     {Fore.CYAN}5. Exit                          {Fore.GREEN}║
 {Fore.GREEN}║     {Fore.CYAN}developed by MINADO              {Fore.GREEN}║
 {Fore.GREEN}║                                      ║
 {Fore.GREEN}╚══════════════════════════════════════╝
@@ -62,6 +64,7 @@ def count_followers(username, password):
         print(f"An error occurred: {str(e)}")
 
 
+
 def find_unfollowers(username, password):
     try:
         loader = Instaloader()
@@ -79,11 +82,13 @@ def find_unfollowers(username, password):
             for user in tqdm(unfollowers, ncols=80):
                 time.sleep(0.1)  # Simulate some work
                 print(f"{COLOR_UNFOLLOWERS}{user.username} - Did not follow you back")
+
         else:
             print(f"{COLOR_UNFOLLOWERS}No users found.")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+
 
 
 def count_following(username, password):
@@ -100,13 +105,35 @@ def count_following(username, password):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
+def unfollow_all_users(username, password):
+    try:
+        # Create a client instance and login
+        api = Client(username, password)
+        api.login()
+
+        # Get the list of users the account is following
+        rank_token = api.generate_uuid()
+        following = api.user_following(api.authenticated_user_id, rank_token=rank_token)
+
+        # Unfollow each user
+        for user in following['users']:
+            api.friendships_destroy(user['pk'])
+            print(f"{COLOR_SUCCESS}Unfollowed user: {user['username']}")
+
+        print(f"{COLOR_SUCCESS}Unfollowed all users successfully.")
+
+    except Exception as e:
+        print(f"{COLOR_ERROR}An error occurred: {str(e)}")
+
+
+
 
 def main():
     clear_screen()
     print(menu_design)
     print("If your account has 2FA enabled, please disable it to avoid issues")
     while True:
-        choice = input("Enter your choice (1-4): ")
+        choice = input("Enter your choice (1-5): ")
         if choice == "1":
             clear_screen()
             count_followers(username, password)
@@ -117,6 +144,9 @@ def main():
             clear_screen()
             find_unfollowers(username, password)
         elif choice == "4":
+            clear_screen()
+            unfollow_all_users(username, password)
+        elif choice == "5":
             print("Exiting...")
             break
         else:
